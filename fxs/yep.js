288b94c493d9f6ae48e73c6041f4ab2aa4e8c28c
Tylik <tylik.stec@gmail.com>
2021-04-23 16:54:43 +0200

    env var

--- fxs/yep.js
+++ fxs/yep.js
@@ -1,6 +1,8 @@
 exports.handler = (event, context, callback) => {
+  let tylik = process.env.TYLIK;
+  console.log(tylik);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: 'yep! yep!!, yep!!!'})
+    body: JSON.stringify({msg: `yep! yep!!, yep!!! ${tylik}`})
   });
 };
